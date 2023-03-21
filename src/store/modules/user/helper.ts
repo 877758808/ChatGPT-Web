d1c68375ae686e53dfce984006da6899f0b83dbb
Emo Feng <zp5fyb@163.com>
2023-03-21 18:05:59 +0800

    [Feature]个人信息修改

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/18116833?s=96&v=4',
+      name: 'Emo',
+      description: 'Star on <a href="https://github.com/877758808/ChatGPT-Web" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
